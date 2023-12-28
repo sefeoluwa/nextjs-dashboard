ae114b8477a97dcb0eb38aabd57f35fe399ec186
sefeoluwa <sefeoluwa01@gmail.com>
2023-12-28 12:00:00 +0100

    Add signIn and AuthError imports

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
   const FormSchema = z.object({
     id: z.string(),
